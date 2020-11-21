3c34d854ad5cb7ddf5c9d1d04cea69a7ae7b9b96
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-11-21 18:53:13 +0200

    Implement tournament selection algorithm

--- src/TournamentSelection.java
+++ src/TournamentSelection.java
@@ -2,10 +2,26 @@ import java.util.ArrayList;
 
 class TournamentSelection<T extends IChromosome> implements ISelectionAlgorithm<T> {
 
+    private int k;
+
+    TournamentSelection(int k) {
+        this.k = k;
+    }
+
     @Override
     public T select(ArrayList<T> population, IFitnessCalculator<T> calculator) {
-        // TODO Auto-generated method stub
-        return null;
+        float best_fitness = -1;
+        T best_chromosome;
+        for (int i = 0; i < k; ++i) {
+            int s = RandomGenerator.randIndex(population);
+            T chromosome = population.get(i);
+            float fitness = calculator.calculate(chromosome);
+            if (fitness > best_fitness) {
+                best_fitness = fitness;
+                best_chromosome = chromosome;
+            }
+        }
+        return chromosome;
     }
 
 }
