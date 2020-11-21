11a76a2cd5ed0593ae02969ac998c2df1a70ea57
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-11-21 18:53:58 +0200

    Modified members, implemented toString() and isValid() functions, added setter(s) and getter(s)

--- src/ChannelAllocationChromosome.java
+++ src/ChannelAllocationChromosome.java
@@ -3,21 +3,60 @@ import java.util.List;
 
 class ChannelAllocationChromosome implements IChromosome {
 
-    public List<Float> genes;
-    ChannelAllocationChromosome() { genes = new ArrayList<>();}
-    ChannelAllocationChromosome(int initialCapacity){ genes = new ArrayList<>(initialCapacity); }
-    ChannelAllocationChromosome(List<Float> rhs) {genes = rhs;}
+    private List<Float> genes;
+    private MetaData metaData;
+
+    ChannelAllocationChromosome(List<Float> rhs, MetaData metaData)
+    {
+        genes = rhs;
+        this.metaData = metaData;
+    }
+
+    ChannelAllocationChromosome(MetaData metaData)
+    {
+        genes = new ArrayList<>();
+        this.metaData = metaData;
+    }
 
     @Override
-    public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
+    public String toString()
+    {
+        String ret = "";
+        ret += "{";
+        for(int i = 0; i < genes.size(); i++)
+        {
+            ret += "["+ i + "]" + " = " + genes.get(i);
+            if(i != genes.size() - 1)
+                ret += ", ";
+        }
+        ret += "}";
+
+        return ret;
     }
 
     @Override
     public boolean isValid() {
-        // TODO Auto-generated method stub
-        return false;
+        if(genes.isEmpty())
+            return false;
+
+        Float sum = 0f;
+        for(Float f : genes)
+            sum += f;
+
+        return sum <= metaData.getTotalBudget();
+    }
+
+    public void setGenes(List<Float> genes) {
+        this.genes = genes;
+    }
+
+    public List<Float> getGenes() {
+        return genes;
+    }
+
+    public Float getGene(int i)
+    {
+        return genes.get(i);
     }
 
 }
